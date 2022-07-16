c9d0c6db34bf287b1a79c3e86a14bb2098ff63cd
GabriellMiranda <gabriel2020miranda@gmail.com>
2022-07-15 21:13:07 -0300

    Erro léxico funcionando

--- lex.yy.c
+++ lex.yy.c
@@ -373,8 +373,8 @@ static void yynoreturn yy_fatal_error ( const char* msg  );
 	(yy_hold_char) = *yy_cp; \
 	*yy_cp = '\0'; \
 	(yy_c_buf_p) = yy_cp;
-#define YY_NUM_RULES 54
-#define YY_END_OF_BUFFER 55
+#define YY_NUM_RULES 55
+#define YY_END_OF_BUFFER 56
 /* This struct is not used in this scanner,
    but its presence is necessary. */
 struct yy_trans_info
@@ -384,7 +384,7 @@ struct yy_trans_info
 	};
 static const flex_int16_t yy_accept[256] =
     {   0,
-        0,    0,   55,   54,    1,    2,   54,   46,   47,   11,
+        0,    0,   56,   54,    1,    2,   54,   46,   47,   11,
         9,   50,   10,   12,   45,   48,   49,   43,   43,   43,
        43,   43,   43,   54,   43,   43,   43,   43,   43,   43,
        43,   43,   43,   43,   43,   43,   43,   43,   43,   43,
@@ -609,11 +609,11 @@ static const flex_int16_t yy_chk[365] =
     } ;
 
 /* Table of booleans, true if rule could match eol. */
-static const flex_int32_t yy_rule_can_match_eol[55] =
+static const flex_int32_t yy_rule_can_match_eol[56] =
     {   0,
 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,     };
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,     };
 
 static yy_state_type yy_last_accepting_state;
 static char *yy_last_accepting_cpos;
@@ -857,7 +857,7 @@ YY_DECL
 		}
 
 	{
-#line 54 "lex.l"
+#line 53 "lex.l"
 
 
 #line 864 "lex.yy.c"
@@ -929,277 +929,282 @@ do_action:	/* This label is used only to access EOF actions. */
 
 case 1:
 YY_RULE_SETUP
-#line 56 "lex.l"
+#line 55 "lex.l"
 {printf("%s", yytext);}
 	YY_BREAK
 case 2:
 /* rule 2 can match eol */
 YY_RULE_SETUP
-#line 57 "lex.l"
+#line 56 "lex.l"
 {printf("\n%d\t", yylineno);}
 	YY_BREAK
 case 3:
 /* rule 3 can match eol */
 YY_RULE_SETUP
-#line 59 "lex.l"
+#line 58 "lex.l"
 { }
 	YY_BREAK
 case 4:
 YY_RULE_SETUP
-#line 61 "lex.l"
+#line 60 "lex.l"
 {printf("%s", yytext); return(INT);}//OK
 	YY_BREAK
 case 5:
 YY_RULE_SETUP
-#line 62 "lex.l"
+#line 61 "lex.l"
 {printf("%s", yytext); return(FLOAT);}//OK
 	YY_BREAK
 case 6:
 YY_RULE_SETUP
-#line 63 "lex.l"
+#line 62 "lex.l"
 {printf("%s", yytext); return(DOUBLE);}//OK
 	YY_BREAK
 case 7:
 YY_RULE_SETUP
-#line 64 "lex.l"
+#line 63 "lex.l"
 {printf("%s", yytext); return(CHAR);}//OK
 	YY_BREAK
 case 8:
 YY_RULE_SETUP
-#line 65 "lex.l"
+#line 64 "lex.l"
 {printf("%s", yytext); return(STRUCT);}
 	YY_BREAK
 case 9:
 YY_RULE_SETUP
-#line 67 "lex.l"
+#line 66 "lex.l"
 {printf("%s", yytext); return(PLUS);}//OK
 	YY_BREAK
 case 10:
 YY_RULE_SETUP
-#line 68 "lex.l"
+#line 67 "lex.l"
 {printf("%s", yytext); return(MINUS);}//OK
 	YY_BREAK
 case 11:
 YY_RULE_SETUP
-#line 69 "lex.l"
+#line 68 "lex.l"
 {printf("%s", yytext); return(MULT);}//OK
 	YY_BREAK
 case 12:
 YY_RULE_SETUP
-#line 70 "lex.l"
+#line 69 "lex.l"
 {printf("%s", yytext); return(DIV);}//OK
 	YY_BREAK
 case 13:
 YY_RULE_SETUP
-#line 71 "lex.l"
+#line 70 "lex.l"
 {printf("%s", yytext); return(EXP);}//OK
 	YY_BREAK
 case 14:
 YY_RULE_SETUP
-#line 73 "lex.l"
+#line 72 "lex.l"
 {printf("%s", yytext); return(FALSE);}//OK
 	YY_BREAK
 case 15:
 YY_RULE_SETUP
-#line 74 "lex.l"
+#line 73 "lex.l"
 {printf("%s", yytext); return(TRUE);}//OK
 	YY_BREAK
 case 16:
 YY_RULE_SETUP
-#line 77 "lex.l"
+#line 76 "lex.l"
 {printf("%s", yytext); return(CONTINUE);}
 	YY_BREAK
 case 17:
 YY_RULE_SETUP
-#line 78 "lex.l"
+#line 77 "lex.l"
 {printf("%s", yytext); return(TYPEDEF);}
 	YY_BREAK
 case 18:
 YY_RULE_SETUP
-#line 80 "lex.l"
+#line 79 "lex.l"
 {printf("%s", yytext); return(LEIAME);}
 	YY_BREAK
 case 19:
 YY_RULE_SETUP
-#line 81 "lex.l"
+#line 80 "lex.l"
 {printf("%s", yytext); return(ILAVAMOSNOS);}//OK
 	YY_BREAK
 case 20:
 YY_RULE_SETUP
-#line 82 "lex.l"
+#line 81 "lex.l"
 {printf("%s", yytext); return(RECEBA);}//OK
 	YY_BREAK
 case 21:
 YY_RULE_SETUP
-#line 83 "lex.l"
+#line 82 "lex.l"
 {printf("%s", yytext); return(MENORQUE);}//OK
 	YY_BREAK
 case 22:
 YY_RULE_SETUP
-#line 84 "lex.l"
+#line 83 "lex.l"
 {printf("%s", yytext); return(MAIORQUE);}//OK
 	YY_BREAK
 case 23:
 YY_RULE_SETUP
-#line 85 "lex.l"
+#line 84 "lex.l"
 {printf("%s", yytext); return(GEMEAS);}//OK
 	YY_BREAK
 case 24:
 YY_RULE_SETUP
-#line 86 "lex.l"
+#line 85 "lex.l"
 {printf("%s", yytext); return(NOTIGUAL);}//OK
 	YY_BREAK
 case 25:
 YY_RULE_SETUP
-#line 87 "lex.l"
+#line 86 "lex.l"
 {printf("%s", yytext); return(MENORIGUALQUE);}//OK
 	YY_BREAK
 case 26:
 YY_RULE_SETUP
-#line 88 "lex.l"
+#line 87 "lex.l"
 {printf("%s", yytext); return(MAIORIGUALQUE);}//OK
 	YY_BREAK
 case 27:
 YY_RULE_SETUP
-#line 89 "lex.l"
+#line 88 "lex.l"
 {printf("%s", yytext); return(II);}//OK
 	YY_BREAK
 case 28:
 YY_RULE_SETUP
-#line 90 "lex.l"
+#line 89 "lex.l"
 {printf("%s", yytext); return(OU);}//OK
 	YY_BREAK
 case 29:
 YY_RULE_SETUP
-#line 92 "lex.l"
+#line 91 "lex.l"
 {printf("%s", yytext); return(RETURN);}//OK
 	YY_BREAK
 case 30:
 YY_RULE_SETUP
-#line 93 "lex.l"
+#line 92 "lex.l"
 {printf("%s", yytext); return(PRINTF);}
 	YY_BREAK
 case 31:
 YY_RULE_SETUP
-#line 94 "lex.l"
+#line 93 "lex.l"
 {printf("%s", yytext); return(FOR);}
 	YY_BREAK
 case 32:
 YY_RULE_SETUP
-#line 95 "lex.l"
+#line 94 "lex.l"
 {printf("%s", yytext); return(WHILE);}
 	YY_BREAK
 case 33:
 YY_RULE_SETUP
-#line 96 "lex.l"
+#line 95 "lex.l"
 {printf("%s", yytext); return(IF);}//OK
 	YY_BREAK
 case 34:
 YY_RULE_SETUP
-#line 97 "lex.l"
+#line 96 "lex.l"
 {printf("%s", yytext); return(ELSE);}//OK
 	YY_BREAK
 case 35:
 YY_RULE_SETUP
-#line 98 "lex.l"
+#line 97 "lex.l"
 {printf("%s", yytext); return(CASE);}
 	YY_BREAK
 case 36:
 YY_RULE_SETUP
-#line 99 "lex.l"
+#line 98 "lex.l"
 {printf("%s", yytext); return(SWITCH);}
 	YY_BREAK
 case 37:
 YY_RULE_SETUP
-#line 100 "lex.l"
+#line 99 "lex.l"
 {printf("%s", yytext); return(GOTO);}
 	YY_BREAK
 case 38:
 YY_RULE_SETUP
-#line 101 "lex.l"
+#line 100 "lex.l"
 {printf("%s", yytext); return(MALLOC);}
 	YY_BREAK
 case 39:
 YY_RULE_SETUP
-#line 102 "lex.l"
+#line 101 "lex.l"
 {printf("%s", yytext); return(SIZEOF);}
 	YY_BREAK
 case 40:
 YY_RULE_SETUP
-#line 103 "lex.l"
+#line 102 "lex.l"
 {printf("%s", yytext); return(PONTEIRO);}
 	YY_BREAK
 case 41:
 YY_RULE_SETUP
-#line 104 "lex.l"
+#line 103 "lex.l"
 {printf("%s", yytext); return(BREAK);}
 	YY_BREAK
 case 42:
 YY_RULE_SETUP
-#line 105 "lex.l"
+#line 104 "lex.l"
 {printf("%s", yytext); return(DO);}
 	YY_BREAK
 case 43:
 YY_RULE_SETUP
-#line 106 "lex.l"
+#line 105 "lex.l"
 {printf("%s", yytext); return(ID);}//OK
 	YY_BREAK
 case 44:
 YY_RULE_SETUP
-#line 107 "lex.l"
+#line 106 "lex.l"
 {printf("%s", yytext); return NUMERO;}//OK
 	YY_BREAK
 case 45:
 YY_RULE_SETUP
-#line 108 "lex.l"
+#line 107 "lex.l"
 {printf("%s", yytext); return NUMERO;}//OK
 	YY_BREAK
 case 46:
 YY_RULE_SETUP
-#line 110 "lex.l"
+#line 109 "lex.l"
 {printf("%s", yytext); return('(');}//OK
 	YY_BREAK
 case 47:
 YY_RULE_SETUP
-#line 111 "lex.l"
+#line 110 "lex.l"
 {printf("%s", yytext); return(')');}//OK
 	YY_BREAK
 case 48:
 YY_RULE_SETUP
-#line 112 "lex.l"
+#line 111 "lex.l"
 {printf("%s", yytext); return(':');}
 	YY_BREAK
 case 49:
 YY_RULE_SETUP
-#line 113 "lex.l"
+#line 112 "lex.l"
 {printf("%s", yytext); return(';');}//OK
 	YY_BREAK
 case 50:
 YY_RULE_SETUP
-#line 114 "lex.l"
+#line 113 "lex.l"
 {printf("%s", yytext); return(',');}
 	YY_BREAK
 case 51:
 YY_RULE_SETUP
-#line 115 "lex.l"
+#line 114 "lex.l"
 {printf("%s", yytext); return('{');}//OK
 	YY_BREAK
 case 52:
 YY_RULE_SETUP
-#line 116 "lex.l"
+#line 115 "lex.l"
 {printf("%s", yytext); return('}');}//OK
 	YY_BREAK
 case 53:
 YY_RULE_SETUP
-#line 119 "lex.l"
+#line 118 "lex.l"
 {printf("%s", yytext); return(STR);}//OK
 	YY_BREAK
 case 54:
 YY_RULE_SETUP
 #line 120 "lex.l"
+{yyerror_lex(yytext);}
+	YY_BREAK
+case 55:
+YY_RULE_SETUP
+#line 121 "lex.l"
 ECHO;
 	YY_BREAK
-#line 1203 "lex.yy.c"
+#line 1208 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -2216,11 +2221,12 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 120 "lex.l"
+#line 121 "lex.l"
 
 
 void yyerror_lex(char *s) {
-    printf("\n\nErro léxico próximo a linha %d: %s\n", yylineno, s);
+    printf("\n\nErro léxico próximo a linha %d, Lexema não reconhecido: %s\n", yylineno, s);
+    exit(0);
 }
 
 int yywarp(void){
